dc81c44a09cc00ac9a0522d1d954409f96a628bc
Isaac Puguliese <isaacpugliese2010@gmail.com>
2020-09-20 13:23:48 -0300

    terminado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("| Isaac Pugliese Pereira |");
+            Console.WriteLine("+------------------------+");
+            Console.ResetColor();
+        }
+    }
+}
